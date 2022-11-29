599bf023bf3e3866aa9bfe5884d71e7ca0010696
PrinanSil <78605194+PrinanSil@users.noreply.github.com>
2022-11-30 00:26:58 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,20 @@
+<!--Credits-->
+<section class="py-2 bg-dark text-white text-center">
+        <div class="container-fluid">
+            A Website By Prinan Sil
+        </div>
+    </section>
+<!--/Credits-->
+
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
+<!-- MDB -->
+<!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.5.0/mdb.min.js"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
